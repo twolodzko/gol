ab1981a4d2fe79c7537880ec9761dae4ee576f8c
Tymoteusz Wołodźko <tymoteusz.wolodzko@greensteam.com>
2021-03-30 19:12:28 +0200

    Booleans

--- evaluator/buildins.go
+++ evaluator/buildins.go
@@ -8,10 +8,18 @@ import (
 
 type buildin = func(objects.List) (objects.Object, error)
 
+var (
+	True  = objects.Symbol{Val: "true"}
+	False = objects.Symbol{Val: "false"}
+)
+
 var buildins = map[string]buildin{
 	"str":   fixedNumArgs(toString, 1),
 	"int":   fixedNumArgs(toInt, 1),
 	"float": fixedNumArgs(toFloat, 1),
+	"true?": fixedNumArgs(isTrue, 1),
+	"not":   fixedNumArgs(notTrue, 1),
+	"list":  func(o objects.List) (objects.Object, error) { return o, nil },
 }
 
 func fixedNumArgs(fn func(objects.Object) (objects.Object, error), numArgs int) buildin {
@@ -22,4 +30,19 @@ func fixedNumArgs(fn func(objects.Object) (objects.Object, error), numArgs int) 
 			return nil, errors.New("wrong number of arguments")
 		}
 	}
+}
+
+func isTrue(o objects.Object) (objects.Object, error) {
+	// FIXME: need to evaluate the symbols!
+	if o == False {
+		return False, nil
+	}
+	return True, nil
+}
+
+func notTrue(o objects.Object) (objects.Object, error) {
+	if o == False {
+		return True, nil
+	}
+	return False, nil
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,7 +14,6 @@ func TestEval(t *testing.T) {
 		input    string
 		expected objects.Object
 	}{
-		// atoms
 		{"()", objects.List{}},
 		{"2", objects.Int{Val: 2}},
 		{"3.14", objects.Float{Val: 3.14}},
@@ -23,6 +22,43 @@ func TestEval(t *testing.T) {
 		{`(str 3.14)`, objects.String{Val: "3.14"}},
 		{`(int "3.14")`, objects.Int{Val: 3}},
 		{`(float "1")`, objects.Float{Val: 1}},
+		{`(list "Hello World!" 42 3.14)`, objects.NewList(objects.String{Val: "Hello World!"}, objects.Int{Val: 42}, objects.Float{Val: 3.14})},
+	}
+
+	for _, tt := range testCases {
+		expr, err := parser.Parse(strings.NewReader(tt.input))
+
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+
+		result, err := Eval(expr[0])
+
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+		if !cmp.Equal(result, tt.expected) {
+			t.Errorf("expected: %v (%T), got: %s (%T)", tt.expected, tt.expected, result, result)
+		}
+	}
+}
+
+func TestBooleans(t *testing.T) {
+	var testCases = []struct {
+		input    string
+		expected objects.Object
+	}{
+		// booleans: everything is true, unless it's false
+		{`(true? "1")`, True},
+		{`(true? 0)`, True},
+		{`(true? 3.1415)`, True},
+		{`(true? foo)`, True},
+		{`(true? true)`, True},
+		{`(true? ())`, True},
+		{`(true? false)`, False},
+		{`(not true)`, False},
+		{`(not false)`, True},
+		{`(not ())`, False},
 	}
 
 	for _, tt := range testCases {
